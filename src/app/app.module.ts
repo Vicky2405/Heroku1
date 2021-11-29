76943988241c5ae71c97c80a0c4754c71cd08c46
Vivek Rajkumar <vivek@Rs-MacBook-Pro.local>
2021-11-29 14:54:54 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,27 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BtStrapComponent } from './boots.component';
 import { FormsComponent } from './forms/forms.component';
-import { NgModelComponent } from './forms/NgModel.component';
-import { FormValidComponent } from './forms/FormValid.component';
-import { FrValidComponent } from './forms/FormValid1.component';
-import { SpecificComponent } from './forms/Specific.component';
-import { PValidComponent } from './forms/PValidation.component';
-import { NgModelGrpComponent } from './forms/NgFormGrp.component';
-import { PNgModelGrpComponent } from './forms/PNgFormGrp.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     BtStrapComponent,
-    FormsComponent,
-    NgModelComponent,
-    FormValidComponent,
-    FrValidComponent,
-    SpecificComponent,
-    PValidComponent,
-    NgModelGrpComponent,
-    PNgModelGrpComponent
+    FormsComponent
   ],
   imports: [
     BrowserModule,
